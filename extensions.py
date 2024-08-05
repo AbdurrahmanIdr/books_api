0a8c464e76a599eeebc76cd41b1719ca20f43a50
Abdurrahman Idris <abdurrahmaneedrees@gmail.com>
2024-08-05 23:35:55 +0100

    add: initialized jwt module

--- extensions.py
+++ extensions.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
 
 class Base(DeclarativeBase):
@@ -10,3 +11,5 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 
 migrate = Migrate()
+
+jwt = JWTManager()
