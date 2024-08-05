0d520cd04cac68020ddd0503c52bff273f4778b0
Abdurrahman Idris <abdurrahmaneedrees@gmail.com>
2024-08-05 23:33:36 +0100

    feat: initialized jwt token based auth on the project
    add: imported user blueprint for responding to api under user

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
-from flask import Flask
-from extensions import db, migrate
+from flask import Flask, jsonify
+from extensions import db, migrate, jwt
 from secrets import token_urlsafe
 from api.books import books_bp
 from api.auth import auth_bp
+from api.users import users_bp
 
 
 def create_app():
@@ -17,10 +18,12 @@ def create_app():
 
     app.config.from_prefixed_env()
     app.config['SECRET_KEY'] = token_urlsafe(32)
+    app.config['JWT_SECRET_KEY'] = token_urlsafe(32)
 
     # initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
+    jwt.init_app(app)
 
     with app.app_context():
         db.create_all()
@@ -28,6 +31,29 @@ def create_app():
     # register blueprints
     app.register_blueprint(books_bp)
     app.register_blueprint(auth_bp)
+    app.register_blueprint(users_bp)
+
+    # jwt error handlers
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_data):
+        return jsonify({
+            "message": "Token expired",
+            "expired": True
+        }), 401
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return jsonify({
+            "message": "Signature verification failed",
+            "error": "Token is invalid"
+        }), 401
+
+    @jwt.unauthorized_loader
+    def unauthorized_callback(error):
+        return jsonify({
+            "message": "Request does not contain valid token",
+            "error": "Unauthorized"
+        }), 401
 
     return app
 
