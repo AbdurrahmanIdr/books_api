f1efb93885c658fc372dc18f4cd339e336de1df2
Abdurrahman Idris <abdurrahmaneedrees@gmail.com>
2024-08-05 23:34:58 +0100

    feat: added login route that incoporates with jwt access and expired token on successful

--- api/auth.py
+++ api/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from flask_jwt_extended import create_access_token, create_refresh_token
 from models.user import User
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/api/v1/auth')
@@ -9,14 +10,18 @@ def register():
     data = request.get_json()
 
     username = data.get('username', None)
+    email = data.get('email', None)
+    password = data.get('password', None)
+
     if username is None:
         return jsonify({'message': 'Username is required'}), 400
 
-    user = User.get_user_by_username(username=username)
-    if user is None:
-        email = data.get('email', None)
-        password = data.get('password', None)
+    if User.check_email_exists(email=email):
+        return jsonify({'message': 'Email already exists'}), 409
 
+    user = User.get_user_by_username(username=username)
+
+    if user is None:
         user = User()
         user.username = username
         user.email = email
@@ -26,3 +31,27 @@ def register():
         return jsonify({'message': 'User created'}), 201
 
     return jsonify({'message': 'User with that username already exist'}), 409
+
+
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username', None)
+    password = data.get('password', None)
+
+    if username is None or password is None:
+        return jsonify({'error': 'Username/Password required'}), 400
+
+    user = User.get_user_by_username(username=username)
+
+    if not (user and user.check_password(password)):
+        return jsonify({'error': 'Invalid username or password'}), 400
+
+    access_token = create_access_token(identity=username)
+    refresh_token = create_refresh_token(identity=username)
+
+    return jsonify({
+        'message': 'Login successful',
+        'access_token': access_token,
+        'refresh_token': refresh_token
+        }), 200
