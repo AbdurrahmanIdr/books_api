ed1a178839543e70ae5aaed75e83181e5b03a94f
Abdurrahman Idris <abdurrahmaneedrees@gmail.com>
2024-08-06 16:30:33 +0100

    add: added refresh token route, whoami route and logout route that uses TokenBlocklist model to revoke logout user

--- api/auth.py
+++ api/auth.py
@@ -1,6 +1,11 @@
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (create_access_token,
+                                create_refresh_token,
+                                jwt_required, get_jwt,
+                                current_user
+                                )
 from models.user import User
+from models.token import TokenBlocklist
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/api/v1/auth')
 
@@ -54,4 +59,31 @@ def login():
         'message': 'Login successful',
         'access_token': access_token,
         'refresh_token': refresh_token
-        }), 200
+    }), 200
+
+
+@auth_bp.get('/whoami')
+@jwt_required()
+def whoami():
+    return jsonify({
+        'message': f'Welcome {current_user.username}',
+        'user details': current_user.to_dict()
+    }), 200
+
+
+@auth_bp.route('/refresh', methods=['GET'])
+@jwt_required(refresh=True)
+def refresh():
+    identity = get_jwt().get('sub')
+    access_token = create_access_token(identity=identity)
+    return jsonify({"access_token": access_token})
+
+
+@auth_bp.get('/logout')
+@jwt_required(verify_type=False)
+def logout():
+    jti = get_jwt()['jti']
+    token_type = get_jwt()['type']
+    token_b = TokenBlocklist(jti=jti)
+    token_b.save()
+    return jsonify({'message': f'{token_type} has been revoked successfully'}), 200
